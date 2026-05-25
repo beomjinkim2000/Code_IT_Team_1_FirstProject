175b74202b167d7d59cd8179892f271eef21f6e2
YuJY9897 <duffkaus29@gmail.com>
2026-05-25 21:31:49 +0900

    [#15] train loop 구현 (engine/train.py) (#39)
    * add test/check_yolo.py bus.jpg yolov8n.pt
    
    * create baseline.py
    
    * num_classes 이상값 방어 코드 추가
    
    * kaiming code add
    
    * review바탕 코드 수정
    
    * train.py 구현
    
    * tests/bus.jpg, check_yolo.py 삭제
    
    * build model 수정, from src.utils.config import load_config 추가
    
    ---------
    
    Co-authored-by: beomjinkim2000 <beomjinkim2000@gmail.com>

--- src/models/baseline.py
+++ src/models/baseline.py
@@ -1,34 +1,35 @@
 import torch
 from torch import nn
 from ultralytics import YOLO
-
 from src.utils.config import load_config
 
 
+# YOLO모델 기존에 학습된 클래스를 필요한 클래스(알약) 수에 맞게 변경하는 함수
 def _adapt_num_classes(model: nn.Module, num_classes: int) -> None:
-    # YOLO 프리트레인 Detect Head를 알약 클래스 수에 맞게 교체
 
-    detect_head = model.model[-1]  # 마지막 레이어 = Detect Head
-    detect_head.nc = num_classes
-    detect_head.no = num_classes + detect_head.reg_max * 4  # reg_max * 4: 4좌표(l/t/r/b) 각각 reg_max개 DFL 분포
+    detect_head = model.model[-1]       #YOLO 모델 마지막에 있는 Detect Head를 가져옴
+    detect_head.nc = num_classes        #Detect Head의 클래스 수를 num_classes로 변경  (config.yaml의 num_classes와 일치)
+    detect_head.no = num_classes + detect_head.reg_max * 4      #num_classes + 4개의 bbox 좌표 예측값(left, top, right, bottom) * 4
 
-    for branch in detect_head.cv3:  # 클래스 예측 브랜치 3개 (P3/P4/P5)
-        last_layer = branch[-1]
+    #각 예측 레이어의 마지막 레이어를 num_classes에 맞게 변경
+    for branch in detect_head.cv3:      #class 예측을 담당하는 레이어 묶음들
+        last_layer = branch[-1]     #마지막 레이어 를 가져옴
         new_layer = nn.Conv2d(
-            out_channels=num_classes,
-            in_channels=last_layer.in_channels,
+            out_channels=num_classes,       #출력 채널만 num_classes로 변경
+            in_channels=last_layer.in_channels,     #기존 설정 동일
             kernel_size=last_layer.kernel_size,
             stride=last_layer.stride,
             padding=last_layer.padding,
             bias=last_layer.bias is not None,
         )
-        nn.init.kaiming_normal_(new_layer.weight, mode="fan_in", nonlinearity="relu")
-        if new_layer.bias is not None:
-            nn.init.zeros_(new_layer.bias)
-        branch[-1] = new_layer
+        nn.init.kaiming_normal_(new_layer.weight, mode="fan_in", nonlinearity="relu")       #채널 입력수 에 맞춰(fan_in) weight 값 조정
+        if new_layer.bias is not None:      #bias가 있는 경우
+            nn.init.zeros_(new_layer.bias)      #bias는 0으로 초기화
+        branch[-1] = new_layer      #new_layer에 마지막 레이어에 변경한 weight, bias값을 저장
 
-    model.nc = num_classes
+    model.nc = num_classes      #모델에 num_classes 정보를 저장
     model.names = {i: str(i) for i in range(num_classes)}
+    #model.names = {i: name for i, name in enumerate(class_names)}      #class_names가 작성되면 해당 코드로 수정
 
 
 def build_model(num_classes: int) -> torch.nn.Module:
